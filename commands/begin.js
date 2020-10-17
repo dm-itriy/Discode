0fffb8392a3b95e6ffc3de32389641b3c89d4e13
Raymond <rpg360@cs.washington.edu>
2020-10-17 15:15:52 -0700

    Changes to begin

--- commands/begin.js
+++ commands/begin.js
@@ -2,6 +2,15 @@ module.exports = {
     name: "begin",
     desription: "starting the bot's functionality",
     execute(message, args) {
-        message.channel.send('Begun!');
+        let time = 20;
+        let timerObj = setInterval(function() {
+            time -= 2;
+            message.channel.send(`${time}`);
+            }, 2000
+        );
+        setTimeout(() => { 
+            clearInterval(timerObj); 
+            message.channel.send(`Finished!`);
+        }, 1000*time);
     }
 }
