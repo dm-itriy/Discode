bc0a2c5d67f5772a59230818f913dfac85600c2e
kumararoosh <arkumar@Aroosh-Computer.localdomain>
2020-10-18 01:19:20 -0700

    added two medium problems

--- remote-code-execution/server/problems.js
+++ remote-code-execution/server/problems.js
@@ -3,7 +3,7 @@
 
 let Problems = [
     {
-        prompt: "Write a program that prints \"Hello World!\"",
+        prompt: "Write a program that prints \"Hello World!\" out to standard output",
         input: "None"
     },
     {
@@ -18,17 +18,22 @@ let Problems = [
         prompt: "You are given __n__ words. Some words may repeat. For each word, output its number of occurrences.\n" + 
         "The output order should correspond with the input order of appearance of the word.\n" +
         "Input Format\n"+
-        "The first line contains the integer, __n__ .\n" +
-        "The next __n__ lines each contain a word.\n" +
+        "The first line in standard input contains the integer, __n__ .\n" +
+        "The next __n__ lines in standard input each contain a word.\n" +
         "Output 2 lines.\n" +
-        "On the first line, output the number of distinct words from the input.\n"+
-        "On the second line, output the number of occurrences for each distinct word according to their appearance in the input.\n",
+        "On the first line, output the number of distinct words to standard output from the input.\n"+
+        "On the second line, output the number of occurrences to standard output for each distinct word according to their appearance in the input.\n",
         input: "4\nbcdef\nabcdefg\nbcde\nbcdef\n"
     },
     {
         prompt: "You are climbing a stair case. It takes n steps to reach to the top.\n" +
-        "Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?\n",
+        "Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?\n"+
+        "You will recieve __n__ through standard input, and must return output to standard output\n",
         input: "3"
+    }, 
+    {
+        prompt: "Given a string s, return the longest palindromic substring in s.\n",
+        input: "cbbd"
     }
 ]
 
